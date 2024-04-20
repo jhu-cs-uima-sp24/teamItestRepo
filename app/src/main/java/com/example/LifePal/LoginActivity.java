00d46631f45dc594c4f07ba0030204bea7f0f112
timothylin <timothylinziqi@gmail.com>
2024-04-20 15:55:22 -0400

    LoginActivity credentials implemented

--- app/src/main/java/com/example/LifePal/LoginActivity.java
+++ app/src/main/java/com/example/LifePal/LoginActivity.java
@@ -6,6 +6,14 @@ import android.widget.EditText;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.LifePal.ui.stats.TagModel;
+import com.google.firebase.Firebase;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
@@ -18,14 +26,36 @@ public class LoginActivity extends AppCompatActivity {
         Button loginButton = findViewById(R.id.loginButton);
         Button signUpButton = findViewById(R.id.signUpButton);
 
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        HashSet<String> allUsernames = new HashSet<>();
+        Map<String, Object> allEntry = new HashMap<>();
+
+        db.collection("allUsersLogins").document("credentials")
+                .get().addOnSuccessListener(documentSnapshot -> {
+                    Map<String, Object> entry = documentSnapshot.getData();
+                    for (String s: entry.keySet()) {
+                        allUsernames.add(s);
+                        allEntry.put(s, entry.get(s));
+                    }
+                });
+
+
+
+
+
         loginButton.setOnClickListener(v -> {
+
+
             String name = usernameEditText.getText().toString();
             String password = passwordEditText.getText().toString();
-            if (!name.isEmpty() && !password.isEmpty()) {
+            if (!name.isEmpty() && !password.isEmpty() && allUsernames.contains(name) && allEntry.get(name).equals(password)) {
                 Toast.makeText(this, "Login successful", Toast.LENGTH_SHORT).show();
                 // Intent to start MainActivity
                 startActivity(new Intent(this, MainActivity.class));
                 finish();
+            } else if (!name.isEmpty() && !password.isEmpty()) {
+                Toast.makeText(this, "Invalid username or password", Toast.LENGTH_SHORT).show();
             } else {
                 Toast.makeText(this, "Please enter email and password", Toast.LENGTH_SHORT).show();
             }
