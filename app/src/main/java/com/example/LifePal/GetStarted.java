9c9c8927c65b1150797912904770d5b8ee922bfe
EricYiming <ericeason2003@gmail.com>
2024-04-20 23:41:21 -0400

    login and signup basically done

--- app/src/main/java/com/example/LifePal/GetStarted.java
+++ app/src/main/java/com/example/LifePal/GetStarted.java
@@ -3,10 +3,13 @@ package com.example.LifePal;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -14,12 +17,19 @@ import androidx.fragment.app.FragmentTransaction;
 import android.content.Intent;
 
 import com.example.LifePal.databinding.FragmentGetStartedBinding;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 
 public class GetStarted extends Fragment {
 
     private FragmentGetStartedBinding binding;
     private SharedPreferences sharedPrefs;
-    Button getStartedButton;
+    private Button loginButton, signUpButton;
+    private EditText usernameInput, passwordInput;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -27,33 +37,60 @@ public class GetStarted extends Fragment {
         binding = FragmentGetStartedBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
         sharedPrefs = getActivity().getSharedPreferences(getString(R.string.storage), Context.MODE_PRIVATE);
-        getStartedButton = binding.getStartedButton;
-        getStartedButton.setOnClickListener(new View.OnClickListener() {
+        loginButton = binding.loginButton;
+        signUpButton = binding.signUpButton;
+        usernameInput = binding.usernameInput;
+        passwordInput = binding.passwordInput;
+
+
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        HashSet<String> allUsernames = new HashSet<>();
+        Map<String, Object> allEntry = new HashMap<>();
+
+        db.collection("allUsersLogins").document("credentials")
+                .get().addOnSuccessListener(documentSnapshot -> {
+                    Map<String, Object> entry = documentSnapshot.getData();
+                    for (String s: entry.keySet()) {
+                        allUsernames.add(s);
+                        allEntry.put(s, entry.get(s));
+                    }
+                });
+
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String goal = sharedPrefs.getString("user_goal", null);
-                if (goal == null) {
                     // Replace the current fragment with the next fragment
-                    Fragment fragment = new EnterName();
-                    FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                    fragmentTransaction.replace(R.id.enter_name, fragment);
-                    fragmentTransaction.addToBackStack(null);
-                    fragmentTransaction.commit();
-                    root.setVisibility(View.GONE);
-                } else {
+                Fragment fragment = new EnterName();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.enter_name, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+                root.setVisibility(View.GONE);
+            }
+        });
+
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                String name = usernameInput.getText().toString();
+                String password = passwordInput.getText().toString();
+                if (!name.isEmpty() && !password.isEmpty() && allUsernames.contains(name) && allEntry.get(name).equals(password)) {
+                    Toast.makeText(getActivity(), "Login successful", Toast.LENGTH_SHORT).show();
+                    // Intent to start MainActivity
                     Intent intent = new Intent(getActivity(), MainActivity.class);
                     startActivity(intent);
-                    /*Fragment fragment = new EnterName(); //for debugging
-                    FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
-                    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                    fragmentTransaction.replace(R.id.get_started, fragment);
-                    fragmentTransaction.addToBackStack(null);
-                    fragmentTransaction.commit();
-                    root.setVisibility(View.GONE);*/
+                } else if (!name.isEmpty() && !password.isEmpty()) {
+                    Toast.makeText(getActivity(), "Invalid username or password", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getActivity(), "Please enter email and password", Toast.LENGTH_SHORT).show();
                 }
-
-
             }
         });
 
