3acfc4005549c19690358915f24fd54a1af1545d
timothylin <timothylinziqi@gmail.com>
2024-04-22 14:11:55 -0400

    solved the second and minute problem 2

--- app/src/main/java/com/example/LifePal/ui/stats/TagModel.java
+++ app/src/main/java/com/example/LifePal/ui/stats/TagModel.java
@@ -13,8 +13,8 @@ public class TagModel {
     // Getters
     public String getName() { return name; }
     public String getTime() {
-        int hours = time/3600;
-        int min = time/60;
-        return Integer.toString(hours) + "H " + Integer.toString(min) + "M";
+        int hours = time/60;
+        int min = time%60;
+        return Integer.toString(hours) + "M " + Integer.toString(min) + "S";
     }
 }
